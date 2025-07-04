aad9a422904967095bb7da81474f15224d46e35a
Siddhesh Thakur <siddheshrjthakur@gmail.com>
2025-07-05 03:04:31 +0530

    Fix Express version and socket initialization issues

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,7 +9,9 @@ import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { app, server } from "./lib/socket.js";
+import { initializeSocket } from "./lib/socket.js";
+
+const app = express();
 
 dotenv.config();
 
@@ -38,7 +40,10 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-server.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
 });
+
+// Initialize socket.io
+initializeSocket(server);
--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -1,50 +1,52 @@
 import { Server } from "socket.io";
 import http from "http";
-import express from "express";
 
-const app = express();
-const server = http.createServer(app);
+let io;
+let server;
 
-const io = new Server(server, {
-    cors: {
-        origin: ["http://localhost:5173"]
-    },
-});
+export function initializeSocket(httpServer) {
+    server = httpServer;
+    io = new Server(server, {
+        cors: {
+            origin: ["http://localhost:5173"]
+        },
+    });
+    
+    io.on("connection", (socket) => {
+        console.log("A user connected", socket.id);
 
-export function getReceiverSocketId(userId) {
-    return userSocketMap[userId];
+        const userId = socket.handshake.query.userId;
+        console.log("User ID from query:", userId);
+        
+        if (userId) {
+            userSocketMap[userId] = socket.id;
+            console.log("User added to online map:", userId);
+            console.log("Current online users:", Object.keys(userSocketMap));
+        }
+
+        //io.emit() is userd to send events to all the connected clients
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+        console.log("Emitted online users:", Object.keys(userSocketMap));
+
+        socket.on("disconnect", () => {
+            console.log("A user disconnected", socket.id);
+            // Find and remove the user from userSocketMap
+            const disconnectedUserId = Object.keys(userSocketMap).find(key => userSocketMap[key] === socket.id);
+            if (disconnectedUserId) {
+                delete userSocketMap[disconnectedUserId];
+                console.log("User removed from online map:", disconnectedUserId);
+            }
+            io.emit("getOnlineUsers", Object.keys(userSocketMap));
+            console.log("Emitted updated online users:", Object.keys(userSocketMap));
+        });
+    });
 }
 
 // used to store online users
 const userSocketMap = {}; // {userId: socketId}
 
-io.on("connection", (socket) => {
-    console.log("A user connected", socket.id);
+export function getReceiverSocketId(userId) {
+    return userSocketMap[userId];
+}
 
-    const userId = socket.handshake.query.userId;
-    console.log("User ID from query:", userId);
-    
-    if (userId) {
-        userSocketMap[userId] = socket.id;
-        console.log("User added to online map:", userId);
-        console.log("Current online users:", Object.keys(userSocketMap));
-    }
-
-    //io.emit() is userd to send events to all the connected clients
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    console.log("Emitted online users:", Object.keys(userSocketMap));
-
-    socket.on("disconnect", () => {
-        console.log("A user disconnected", socket.id);
-        // Find and remove the user from userSocketMap
-        const disconnectedUserId = Object.keys(userSocketMap).find(key => userSocketMap[key] === socket.id);
-        if (disconnectedUserId) {
-            delete userSocketMap[disconnectedUserId];
-            console.log("User removed from online map:", disconnectedUserId);
-        }
-        io.emit("getOnlineUsers", Object.keys(userSocketMap));
-        console.log("Emitted updated online users:", Object.keys(userSocketMap));
-    });
-});
-
-export { io, app, server };
+export { io, server };
